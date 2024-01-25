56863e197ec62dfc400584d6b656dba17c09b6f9
Hoon9901 <shonn.dev@gmail.com>
2024-01-25 21:17:18 +0900

    feat: MemberRepository Added

---
+++ src/main/java/com/megathon/gidung/challenge/repository/ChallengeRepository.java
@@ -0,0 +1,8 @@
+package com.megathon.gidung.challenge.repository;
+
+import com.megathon.gidung.challenge.entity.Challenge;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
+
+}
