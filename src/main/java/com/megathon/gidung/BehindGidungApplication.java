70ae389bff671e0fc9d5464d979bf84d0364aa31
Hoon9901 <shonn.dev@gmail.com>
2024-01-25 16:49:01 +0900

    init projects

---
+++ src/main/java/com/megathon/gidung/BehindGidungApplication.java
@@ -0,0 +1,13 @@
+package com.megathon.gidung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BehindGidungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BehindGidungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/megathon/gidung/BehindGidungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megathon.gidung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BehindGidungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
