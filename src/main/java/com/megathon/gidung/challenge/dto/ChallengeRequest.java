f3f3aa1773ef611c5ba8751c2ac7bfff75cf0d56
Hoon9901 <shonn.dev@gmail.com>
2024-01-25 23:26:20 +0900

    fix: Type simple error fixed
    - 중접 클래스에 대한 static 활성화

--- src/main/java/com/megathon/gidung/challenge/dto/ChallengeRequest.java
+++ src/main/java/com/megathon/gidung/challenge/dto/ChallengeRequest.java
@@ -8,11 +8,13 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
+@Getter
+@Setter
 public class ChallengeRequest {
 
     @Getter
     @Setter
-    public class Create {
+    public static class Create {
         @NotBlank(message = "제목을 입력해주세요.")
         private String title;
 
@@ -33,7 +35,7 @@ public class ChallengeRequest {
 
     @Getter
     @Setter
-    public class Update {
+    public static class Update {
         @NotBlank(message = "제목을 입력해주세요.")
         private String title;
 
